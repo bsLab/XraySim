ad3a2cc3611d392d17865afe3dac950535b35fbb
Priv.-Doz. Dr. Stefan Bosse <41194092+bsLab@users.noreply.github.com>
2023-10-13 12:53:26 +0200

    Fri 13 Oct 2023 12:53:03 PM CEST

---
+++ fbp/cmake.opencv4.sh
@@ -0,0 +1,2 @@
+cmake -D BUILD_opencv_java=OFF -D BUILD_opencv_python=OFF -D BUILD_opencv_python3=OFF -DCMAKE_INSTALL_PREFIX=/opt/opencv  -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.x/modules ../
+
